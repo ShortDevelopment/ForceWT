bfb3dc2e0eeba5ceb3e62db3c1958d2bf5b058a9
Lukas Kurz <vb.net@kurzweb.de>
2021-05-24 18:33:06 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,218 @@
+﻿
+#define USE_DEBUGGER
+//#define REDIRECT_STD
+// #define USE_UTF8
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ForceWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string applicationName = "powershell";
+            if (args.Length > 0)
+                applicationName = Path.GetFileNameWithoutExtension(args[0]).ToLower();
+
+            // copyright
+            Console.WriteLine("Force Windows Terminal");
+            Console.WriteLine($"© {DateTime.Now.ToString("yyyy")} Lukas Kurz alias ShortDevelopment");
+            Console.WriteLine();
+
+#if DEBUG
+            // output args
+            Console.Write("Args: ");
+            Console.WriteLine(FormatArgs(args));
+#endif
+
+#if USE_UTF8
+            // set encoding to utf-8 ⚠ Font may change ❗
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+#endif
+
+            // set console title
+            switch (applicationName.ToLower())
+            {
+                case "powershell":
+                    Console.Title = "Windows PowerShell";
+                    break;
+                case "cmd":
+                    Console.Title = "Eingabeaufforderung";
+                    break;
+                default:
+                    throw new ArgumentException("Unkown application!");
+            }
+
+            // redirect handles
+            STARTUPINFO si = new STARTUPINFO();
+            si.cb = Marshal.SizeOf(si);
+#if REDIRECT_STD
+            si.hStdError = GetStdHandle(STD.OUTPUT_HANDLE);
+            si.hStdOutput = GetStdHandle(STD.OUTPUT_HANDLE);
+            si.hStdInput = GetStdHandle(STD.INPUT_HANDLE);
+            si.dwFlags = STARTF_USESTDHANDLES;
+#endif
+
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+            // start process in debugging mode to ignore "Image File Execution"
+            uint flags = DEBUG_ONLY_THIS_PROCESS;
+#if !USE_DEBUGGER
+            flags = 0;
+#endif
+            if (!CreateProcess(null, (args.Length > 0) ? FormatArgs(args) : applicationName, IntPtr.Zero, IntPtr.Zero, true, flags, IntPtr.Zero, null, ref si, out pi))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            // dummy debugger loop
+            DEBUG_EVENT debugEvent = new DEBUG_EVENT();
+            bool isDebugging = true;
+            while (isDebugging && WaitForDebugEvent(ref debugEvent, INFINITE))
+            {
+                if (debugEvent.dwDebugEventCode == EXIT_PROCESS_DEBUG_EVENT)
+                {
+                    isDebugging = false;
+                }
+                // always continue
+                ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, ContinueStatus.DBG_CONTINUE);
+            }
+
+            // wait for process to terminate
+            WaitForSingleObject(pi.hProcess, INFINITE);
+
+        }
+
+        protected static string FormatArgs(string[] args)
+        {
+            return string.Join(" ", args.Select((arg) => $"\"{arg}\""));
+        }
+
+        #region Security
+        [StructLayout(LayoutKind.Sequential)]
+        protected struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+        #endregion
+
+        #region STD Handle
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(STD nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        protected static extern bool SetStdHandle(STD nStdHandle, IntPtr hHandle);
+        protected enum STD
+        {
+            INPUT_HANDLE = -10,
+            OUTPUT_HANDLE = -11,
+            ERROR_HANDLE = -12
+        }
+        #endregion
+
+        #region CreateProcess
+
+        const int STARTF_USESTDHANDLES = 0x00000100;
+
+        const int CREATE_NO_WINDOW = 0x08000000;
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        protected static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+        // This also works with CharSet.Ansi as long as the calling function uses the same character set.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        protected struct STARTUPINFOEX
+        {
+            public STARTUPINFO StartupInfo;
+            public IntPtr lpAttributeList;
+        }
+
+        // This also works with CharSet.Ansi as long as the calling function uses the same character set.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        protected struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        protected struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+        #endregion
+
+        #region Debug
+        const int DEBUG_PROCESS = 0x00000001;
+        const int DEBUG_ONLY_THIS_PROCESS = 0x00000002;
+
+        const int EXIT_PROCESS_DEBUG_EVENT = 5;
+
+        [DllImport("kernel32.dll", EntryPoint = "WaitForDebugEvent")]
+        protected static extern bool WaitForDebugEvent(ref DEBUG_EVENT lpDebugEvent, uint dwMilliseconds);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEBUG_EVENT
+        {
+            public uint dwDebugEventCode;
+            public int dwProcessId;
+            public int dwThreadId;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 86, ArraySubType = UnmanagedType.U1)]
+            byte[] debugInfo;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ContinueDebugEvent(int dwProcessId, int dwThreadId, ContinueStatus dwContinueStatus);
+        public enum ContinueStatus : uint
+        {
+            DBG_CONTINUE = 0x00010002,
+            DBG_EXCEPTION_NOT_HANDLED = 0x80010001,
+            DBG_REPLY_LATER = 0x40010001
+        }
+        #endregion
+
+        #region Wait
+        [DllImport("kernel32.dll", SetLastError = true)]
+        protected static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        const UInt32 INFINITE = 0xFFFFFFFF;
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# ForceWT
+Some day, this application will open the WindowsTerminal on every attempt of opening `cmd.exe`or `powershell.exe`   
+It works using `HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\`
